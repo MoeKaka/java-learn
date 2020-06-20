a6e0554177ec96dc5318d703ec3291bb7f7ff53b
AKYQ <66791287+AKYQ@users.noreply.github.com>
2020-06-20 17:16:14 +0300

    Greeting from Romania, windows

--- src/main/java/com/java/MainApp.java
+++ src/main/java/com/java/MainApp.java
@@ -3,8 +3,11 @@ package com.java;
 public class MainApp {
 
 	public static void main(String[] args) {
+		// one line comment
 		System.out.println("Hallo World");
 		System.out.println("Hallo from Germany");
+		System.out.println("Hello from Romania windows");
+		
 
 	}
 
