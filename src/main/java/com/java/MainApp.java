f74a9644a2e99d60474f709bc4e7c9e80d749030
moe.kaka2017@gmail.com <mkafa1@hotmail.com>
2020-07-05 17:32:17 +0200

    variables and scanner

--- src/main/java/com/java/MainApp.java
+++ src/main/java/com/java/MainApp.java
@@ -1,13 +1,38 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// one line comment
-		System.out.println("Hallo World");
-		System.out.println("Hallo from Germany");
-		System.out.println("Hello from Romania windows");
-		System.out.println("hello from romania mac");
-	}
 
+		int x, y, z;
+		
+		Scanner input;
+		input = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		x = input.nextInt();
+		System.out.println("Enter second number: ");
+		y = input.nextInt(); // object.method()
+		
+		
+		z = x + y;
+		System.out.print("Result is ");
+		System.out.println(z);
+		
+		
+		/// entering strings
+		String firstName, secondName, fatherName;
+		System.out.print("Enter your first name: ");
+		firstName = input.next();
+		System.out.print("Enter your second name: ");
+		secondName = input.next();
+		System.out.print("Enter your father name: ");
+		fatherName = input.next();
+		
+		String greeting; 
+		greeting = "Hello " + firstName + " " + fatherName + " " + secondName; 
+		System.out.println(greeting);
+		
+	}
 }
